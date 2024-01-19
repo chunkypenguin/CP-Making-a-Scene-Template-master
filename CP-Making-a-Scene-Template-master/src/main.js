ecd1d6a15bd70ee9aac26299068c1fbc975dc8e4
Alex Beteta <abeteta9@gmail.com>
2024-01-18 19:52:19 -0800

    first commit

---
+++ CP-Making-a-Scene-Template-master/src/main.js
@@ -0,0 +1,12 @@
+// Code Practice: Making a Scene
+// Name: Alex Beteta
+// Date: 01-17-2024
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    scene: [ MainMenu, Play ]
+}
+
+let game = new Phaser.Game(config)
